4692a664369f7460e3cd975c1459c640fc24748a
acastemoreno <acastemoreno@gmail.com>
2016-05-27 20:01:08 -0500

    -init presentation

--- 3era session/slide_config.js
+++ 3era session/slide_config.js
@@ -1,13 +1,13 @@
 var SLIDE_CONFIG = {
    // Slide Settings
    settings: {
-     title: 'Org-IO Slide Demo & Example', 
-     subtitle: 'Here Is Subtitle', 
+     title: 'Elixir and Phoenix Framework', 
+     subtitle: '3era Session', 
      useBuilds: true, // Default: true. False will turn off slide animation builds. 
      usePrettify: true, // Default: true 
      enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
      enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-     favIcon: 'images/org-icon.png', 
+     favIcon: 'images/phoenix.svg', 
      fonts: [
        'Open Sans:regular,semibold,italic,italicsemibold',
        'Source Code Pro'
