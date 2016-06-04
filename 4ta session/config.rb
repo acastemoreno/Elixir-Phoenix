47ca571a8ba3dc7a92224f72d614e758727faee1
acastemoreno <acastemoreno@gmail.com>
2016-06-04 06:31:39 -0500

    4ta session avanzada

---
+++ 4ta session/config.rb
@@ -0,0 +1,9 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "theme/css"
+sass_dir = "theme/scss"
+images_dir = "images"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed #:expanded or :nested or :compact or :compressed
